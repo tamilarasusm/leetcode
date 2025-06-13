d00f554fda1635279939ce1c69923b98634ea215
Tamilarasu S M <tamilarasusm@gmail.com>
2025-06-13 19:45:21 +0530

    MergeAlternately Problem

---
+++ programming/src/main/java/in/tamilarasu/leetcode/Main.java
@@ -0,0 +1,10 @@
+package in.tamilarasu.leetcode;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        MergeAlternately mergeAlternately = new MergeAlternately();
+        System.out.println(mergeAlternately.mergeAlternately("abcd", ""));
+    }
+}
